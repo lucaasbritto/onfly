2a7e9edc95045758b3ed3ae1383926e4e3988a42
Lucas Britto <mister.britto@hotmail.com>
2025-07-10 23:11:53 -0300

    feat: Criação do Controller, Model e Migrate do TravelRequest

---
+++ app/app/Http/Controllers/TravelRequestController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TravelRequest;
+use Illuminate\Http\Request;
+
+class TravelRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TravelRequest $travelRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TravelRequest $travelRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TravelRequest $travelRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TravelRequest $travelRequest)
+    {
+        //
+    }
+}
