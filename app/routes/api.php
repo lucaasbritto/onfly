60f9073ae3c163392c4140bb2245c48e9d00875c
Lucas Britto <mister.britto@hotmail.com>
2025-07-12 19:28:20 -0300

    feat: Configuração de autenticação da tela login

---
+++ app/routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->get('/me', [AuthController::class, 'me']);
